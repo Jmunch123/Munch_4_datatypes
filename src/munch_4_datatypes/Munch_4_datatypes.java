e21bf66a69cbd0e185616f74c17e44f7a6164e5d
Jeremy Munch <jmunch4793@stu.neisd.net>
2015-10-05 07:18:53 -0500

    These are the datatypes for CS

---
+++ src/munch_4_datatypes/Munch_4_datatypes.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package munch_4_datatypes;
+
+/**
+ *
+ * @author Jmunch123
+ */
+public class Munch_4_datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //space for comments
+        //used to explain code
+        //code explanation is an expectation
+        //in d22
+        int i = 51;
+        float a = 7.11f;
+        double b = 11.0;
+        short c = 100;
+        char d1 = '1';
+        char d2 = 'd';
+        char d3 = 122;
+        String e = "why certainly";
+        boolean f = false;
+        boolean g = i % 2 == 0;
+        // + this adds two numbers and more
+        // - this subtracts two numbers and more
+        // * this multiplies two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo. Shows you the remainder
+        
+        
+        System.out.println(i + " an integer");
+        System.out.println(a + " a float");
+        System.out.println(b + " a double");
+        System.out.println(c + " a short");
+        System.out.println(d1 + " a char");
+        System.out.println(d2 + " a char");
+        System.out.println(d3 + " a char");
+        System.out.println(e + " a string");
+        System.out.println(f + " a boolean");
+        for(i = 0;i < 500; i++) {
+            System.out.println (i % 4 == 0) ;
+        }
+//        % checks for the remainder
+//         1 % i checks if a number in
+//        even or odd */
+        i = 22;
+        a = 0;
+        e = "ninja";
+         System.out.println(e + i + " an /" + "a string plus an integer");
+         
+        a = 7.11f;
+        b = 11.0;
+         System.out.println(a + b + " a float/" + "a double");
+         
+        
+        for(i = 1;i < 100; i++) {
+            /*if is a coditional (flow) statement if a condition is true 
+            the following command will be executed*/
+            if (i % 2 != 0){
+                 
+                }
+            } 
+//            System.out.println (i + " is even") ;
+         
+             System.out.println(i);
+            }
+        }
+    
+    
+
